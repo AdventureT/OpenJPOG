12ab45485413cf25d8705d879a8b99e0079c7db6
AdventureT <le@nepelius.at>
2023-12-01 18:13:59 +0100

    Added TDLL

---
+++ Toshi/Include/TKernel/TDLL.h
@@ -0,0 +1,32 @@
+#pragma once
+#include <TKernel/TCString.h>
+#include <windows.h>
+
+TOSHI_NAMESPACE_BEGIN
+
+class TOSHI_EXPORT TDLL
+{
+public:
+
+	TDLL()
+	{
+		m_pDLL = TNULL;
+	}
+
+	virtual ~TDLL()
+	{
+		Unload();
+	}
+
+	TPVOID GetAddress(const TCString &a_rProcName);
+	TBOOL Load(const TCString& a_rFileName);
+	void Unload();
+
+	HMODULE GetDLL() { return m_pDLL; }
+	const TCString& GetFilename() { return m_szFilename; }
+private:
+	TCString m_szFilename; // 0x4
+	HMODULE m_pDLL;        // 0xC
+};
+
+TOSHI_NAMESPACE_END
