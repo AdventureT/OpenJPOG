64a85900ed8f42f4468d9f7f69ee5a8d838f4ea9
AdventureT <le@nepelius.at>
2024-08-15 08:13:47 +0200

    Added PPropertyBlock

---
+++ Toshi/Plugins/Include/PPropertyParser/PPropertyBlock.h
@@ -0,0 +1,38 @@
+#pragma once
+#include "Defines.h"
+#include "TKernel/TObject.h"
+#include "TKernel/TPCString.h"
+
+class PPROPERTYPARSER_EXPORTS PPropertyBlock : public Toshi::TObject
+{
+    DECLARE_DYNAMIC(PPropertyBlock);
+
+public:
+    enum Priority
+    {
+
+    };
+
+    PPropertyBlock(const Toshi::TPCString &a_rName, Priority a_ePriority)
+    {
+        m_oName = a_rName;
+        m_ePriority = a_ePriority;
+    }
+
+    void SetName(const Toshi::TPCString &a_rName)
+    {
+        m_oName = a_rName;
+    }
+    const Toshi::TPCString &GetName()
+    {
+        return m_oName;
+    }
+    Priority GetPriority() const
+    {
+        return m_ePriority;
+    }
+
+private:
+    Toshi::TPCString m_oName; // 0x8
+    Priority m_ePriority;     // 0xC
+};
---
+++ Toshi/Plugins/Source/PPropertyParser/PPropertyBlock.cpp
@@ -0,0 +1,6 @@
+#include "PPropertyParser/PPropertyBlock.h"
+
+TOSHI_NAMESPACE_USING
+
+IMPLEMENT_DYNAMIC(PPropertyBlock, TObject);
+
