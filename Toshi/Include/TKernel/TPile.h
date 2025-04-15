810f1b33d35cb69810c1b4019b61fb3732b43c5e
AdventureT <le@nepelius.at>
2025-04-15 10:44:18 +0200

    Implement TPile

--- Toshi/Include/TKernel/TPile.h
+++ Toshi/Include/TKernel/TPile.h
@@ -8,15 +8,25 @@ class TKERNELINTERFACE_EXPORTS TPile
 public:
 	enum PileFlags
 	{
-		PILEFLAG_MALLOC = BITFIELD(0)
+		PILEFLAG_USEBLOCKALIGNMENT = BITFIELD(0)
+	};
+
+	struct Block
+	{
+		Block *m_pNext;
 	};
 
 	TPile(TINT a_iPileSize, TINT a_iBlockSize, TUINT a_iFlags);
+	~TPile()
+	{
+
+	}
 
 	void *RawAlloc(TINT a_iSize, TINT a_iAlignment);
 	TBOOL RawIsInside(void *a_pRaw);
 
 	void Free();
+	void Reset();
 
 	TINT GetNumAllocs() const
 	{
@@ -32,14 +42,17 @@ public:
 	}
 
 private:
-	TINT   m_iPileSize;    // 0x0
-	TINT   m_iBlockSize;   // 0x4
-	TUINT  m_iFlags;       // 0x8
-	TINT   m_iNumBlocks;   // 0xC
-	TINT  *m_pNextBlock;   // 0x14
-	void **m_ppBlocks;     // 0x1C
-	TINT   m_iNumAllocs;   // 0x24
-	TINT   m_iWastedSpace; // 0x28
+	TINT   m_iBlockSize;    // 0x0
+	TINT   m_iMaxBlocks;    // 0x4
+	TUINT  m_iFlags;        // 0x8
+	TINT   m_iNumBlocks;    // 0xC
+	TINT   m_iCurrentBlock; // 0x10
+	void  *m_pBlockStart;   // 0x14
+	void  *m_pBlockEnd;     // 0x18
+	Block *m_pBlocksHead;   // 0x1C
+	Block *m_pBlocksTail;   // 0x20
+	TINT   m_iNumAllocs;    // 0x24
+	TINT   m_iWastedSpace;  // 0x28
 };
 
 TOSHI_NAMESPACE_END
--- Toshi/Source/TKernel/TPile.cpp
+++ Toshi/Source/TKernel/TPile.cpp
@@ -1,33 +1,102 @@
 #include "TPile.h"
+#include "TMemory.h"
 
 TOSHI_NAMESPACE_BEGIN
 
-TPile::TPile(TINT a_iPileSize, TINT a_iSize, TUINT a_iFlags)
+TPile::TPile(TINT a_iBlockSize, TINT a_iSize, TUINT a_iFlags)
 {
-	m_iPileSize    = a_iPileSize;
-	m_iBlockSize   = a_iSize / a_iPileSize;
-	m_iFlags       = a_iFlags;
-	m_iNumBlocks   = 0;
-	m_iNumAllocs   = 0;
-	m_iWastedSpace = 0;
+	m_iBlockSize    = a_iBlockSize;
+	m_iMaxBlocks    = a_iSize / a_iBlockSize;
+	m_iFlags        = a_iFlags;
+	m_iNumBlocks    = 0;
+	m_iCurrentBlock = 0;
+	m_pBlockStart   = TNULL;
+	m_pBlockEnd     = TNULL;
+	m_pBlocksHead   = TNULL;
+	m_pBlocksTail   = TNULL;
+	m_iNumAllocs    = 0;
+	m_iWastedSpace  = 0;
 }
 
 void *TPile::RawAlloc(TINT a_iSize, TINT a_iAlignment)
 {
 	m_iNumAllocs++;
-	return nullptr;
+	TINT uiAlignMask = ~(a_iAlignment - 1);
+	TPCHAR pAligned   = (TPCHAR)(((TUINT)m_pBlockStart + (a_iAlignment - 1)) & uiAlignMask);
+	if (m_pBlockEnd < pAligned + a_iSize) {
+		if (m_iCurrentBlock < m_iNumBlocks) {
+			m_pBlocksTail = m_pBlocksTail->m_pNext;
+		}
+		else {
+			if (m_iMaxBlocks != 0 && m_iMaxBlocks <= m_iNumBlocks) {
+				return TNULL;
+			}
+			Block *pBlock;
+			if ((m_iFlags & PILEFLAG_USEBLOCKALIGNMENT) == 0) {
+				pBlock = (Block *)operator new (m_iBlockSize + 4);
+			}
+			else {
+				const TINT uiBlockAlignment = 32;
+				const TINT uiBlockAlignMask = ~(uiBlockAlignment - 1);
+				pBlock                      = (Block *)tmemalign(uiBlockAlignment, (m_iBlockSize + 4 + (uiBlockAlignment - 1)) & uiBlockAlignMask);
+			}
+			pBlock->m_pNext = TNULL;
+			if (m_pBlocksTail) {
+				m_pBlocksTail->m_pNext = pBlock;
+			}
+			else {
+				m_pBlocksHead = pBlock;
+			}
+			m_pBlocksTail = pBlock;
+			m_iNumBlocks++;
+		}
+		m_pBlockEnd = m_pBlocksTail + m_iBlockSize + 4;
+		m_iCurrentBlock++;
+		pAligned = (TPCHAR)((TUINT)(m_pBlocksTail + 4 + (a_iAlignment - 1)) & uiAlignMask);
+	}
+	m_pBlockStart = pAligned + a_iSize;
+	return pAligned;
 }
 
 TBOOL TPile::RawIsInside(void *a_pRaw)
 {
-	return TBOOL();
+	Block *current = m_pBlocksHead;
+	while (current != TNULL) {
+		if ((current < a_pRaw) && (a_pRaw < current + m_iBlockSize)) {
+			return TTRUE;
+		}
+		current = current->m_pNext;
+	}
+	return TFALSE;
 }
 
 void TPile::Free()
 {
-	for (void *it = m_ppBlocks[0]; it != TNULL; it)
-	{
+	Block *current = m_pBlocksHead;
+	while (current != TNULL) {
+		Block *next = current->m_pNext;
+		delete current;
+		current = next;
 	}
+	m_iNumBlocks = 0;
+	Reset();
+}
+
+void TPile::Reset()
+{
+	if (m_pBlocksHead) {
+		m_pBlockStart    = m_pBlocksHead + 4;
+		m_pBlockEnd     = m_pBlocksHead + m_iBlockSize + 4;
+		m_iCurrentBlock = 1;
+	}
+	else {
+		m_pBlockStart    = TNULL;
+		m_pBlockEnd     = TNULL;
+		m_iCurrentBlock = 0;
+	}
+	m_pBlocksTail  = m_pBlocksHead;
+	m_iNumAllocs   = 0;
+	m_iWastedSpace = 0;
 }
 
 TOSHI_NAMESPACE_END
