b9827c1bb08efbb43b4973ac081f102a5c44233d
AdventureT <le@nepelius.at>
2025-04-18 15:00:57 +0200

    Added PGui to the project

--- Toshi/Plugins/premake5.lua
+++ Toshi/Plugins/premake5.lua
@@ -29,3 +29,35 @@ project ("PPropertyParser")
 			"Include/PPropertyParser/**.h",
 			"Source/PPropertyParser/**.cpp"
 		}
+
+project ("PGui")
+	kind "SharedLib"
+	language "C++"
+	cppdialect "C++20"
+	characterset "ASCII"
+	
+	links
+	{
+		"winmm.lib", 
+		"TKernelInterface"
+	}
+
+	includedirs
+	{
+		"Include",
+		"%{wks.location}/Toshi/Include",
+		"%{wks.location}/Toshi/Include/TKernel"
+	}
+
+	defines
+	{
+		"PGUI"
+	}
+
+	filter "system:windows"
+		files
+		{
+			"Include/*.h",
+			"Include/PGui/**.h",
+			"Source/PGui/**.cpp"
+		}
