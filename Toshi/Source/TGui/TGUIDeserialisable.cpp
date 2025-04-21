3c3c7c1fe1f472da67b4880297aee72eeffbd590
AdventureT <35010415+AdventureT@users.noreply.github.com>
2025-04-21 09:10:53 +0000

    TGUIDeserialisable::Create remove s_flag

--- Toshi/Source/TGui/TGUIDeserialisable.cpp
+++ Toshi/Source/TGui/TGUIDeserialisable.cpp
@@ -6,18 +6,9 @@ IMPLEMENT_DYNAMIC(TGUIDeserialisable, TObject);
 
 TGUIDeserialisable *TOSHI_API TGUIDeserialisable::Create(TGUIInterface *a_pInterface, const PProperties *a_pProperties)
 {
-	static bool          s_flag          = 0;
-	static const TClass *s_pAllowedClass = TNULL;
-	static TPCString     s_propNameType  = TPCString();
-	if ((s_flag & 1) == 0) {
-		s_flag |= 1;
-		s_pAllowedClass = TClass::Find("TGUIDeserialisable", TNULL);
-	}
-	if ((s_flag & 2) == 0) {
-		s_flag |= 2;
-		s_propNameType = TSystem::GetCStringPool()->Get("type");
-	}
-	const PPropertyValue *pProperty = a_pProperties->GetProperty(s_propNameType);
+	static const TClass    *s_pAllowedClass = TClass::Find("TGUIDeserialisable", TNULL);
+	static const TPCString  s_propNameType  = TSystem::GetCStringPool()->Get("type");
+	const PPropertyValue   *pProperty       = a_pProperties->GetProperty(s_propNameType);
 	if (!pProperty) {
 		a_pInterface->Error("Deserialise: Object has no 'type' property specified");
 		return TNULL;
